b4cbaa4d009ef3e58b3acb5ee1363a6f075e4414
naeljeff <naelj15@gmail.com>
2024-08-08 14:23:43 +0700

    finished functionality for category and searchbar in mainpage

--- components/MainPageComponents/Main.jsx
+++ components/MainPageComponents/Main.jsx
@@ -1,12 +1,60 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
 import Header from "../Header";
 import Navbar from "../Navbar";
 import News from "./NewsComponent/News";
 import Footer from "../Footer";
+import {
+  fetchNews,
+  getNews,
+  getNewsLength,
+  getStatus,
+  selectNewsError,
+} from "@/store/slices/newsSlice";
+import { newsList } from "@/utils/newsCategory";
 
 const MainLayout = () => {
+  const [activeCategory, setActiveCategory] = useState(1);
+  const [selectedCategory, setSelectedCategory] = useState(newsList[0].value);
+  const [searchValue, setSearchValue] = useState("");
+  const [page, setPage] = useState(1);
+  const pageSize = 10;
+
+  const dispatch = useDispatch();
+  const articles = useSelector(getNews);
+  const articlesLength = useSelector(getNewsLength);
+  const status = useSelector(getStatus);
+  const error = useSelector(selectNewsError);
+
+  useEffect(() => {
+    dispatch(fetchNews(selectedCategory));
+  }, [dispatch, selectedCategory]);
+
+  useEffect(() => {
+    setPage(1);
+  }, [searchValue, selectedCategory]);
+
+  const fetchMoreData = () => {
+    if (page * pageSize < articlesLength) {
+      setPage((prevPage) => prevPage + 1);
+    }
+  };
+
+  const memoArticles = articles.slice(0, page * pageSize);
+
+  const filteredDataBySearch = () => {
+    if (!searchValue) return memoArticles;
+
+    return memoArticles.filter((article) =>
+      article.title.toLowerCase().includes(searchValue.toLowerCase())
+    );
+  };
+
+  const displayedData = filteredDataBySearch();
+
   return (
     <div className="w-full h-full flex flex-col justify-between items-center bg-[#f8fafc]">
       <div className="w-full flex flex-col justify-start items-start">
@@ -15,12 +63,33 @@ const MainLayout = () => {
         <div className="w-full bg-white">
           <div className="w-4/5 mx-auto border-b border-black" />
         </div>
-        <Navbar />
+        <Navbar
+          active={activeCategory}
+          setActive={setActiveCategory}
+          setSelectedCategory={setSelectedCategory}
+          setSearchValue={setSearchValue}
+        />
       </div>
 
       {/* News Display */}
       <div className="container w-full flex flex-col justify-center items-center lg:w-3/4">
-        <News />
+        {status === "loading" && (
+          <p className="w-full h-full flex justify-center items-center">
+            Loading...
+          </p>
+        )}
+        {status === "failed" && (
+          <p className="w-full h-full flex justify-center items-center">
+            {error}
+          </p>
+        )}
+        {status === "succeeded" && (
+          <News
+            articles={displayedData}
+            fetchMoreData={fetchMoreData}
+            hasMore={page * pageSize < articlesLength}
+          />
+        )}
       </div>
 
       <Footer />
--- components/MainPageComponents/NewsComponent/News.jsx
+++ components/MainPageComponents/NewsComponent/News.jsx
@@ -1,75 +1,33 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import React from "react";
 import InfiniteScroll from "react-infinite-scroll-component";
-import {
-  fetchNews,
-  getNews,
-  getNewsLength,
-  selectNewsError,
-  getStatus,
-} from "@/store/slices/newsSlice";
+
 import NewsCard from "./NewsCard";
 
-const News = () => {
-  const [page, setPage] = useState(1);
-
-  const dispatch = useDispatch();
-  const articles = useSelector(getNews);
-  const articlesLength = useSelector(getNewsLength);
-  const status = useSelector(getStatus);
-  const error = useSelector(selectNewsError);
-  const pageSize = 10;
-
-  useEffect(() => {
-    dispatch(fetchNews("bitcoin"));
-  }, [dispatch]);
-
-  const fetchMoreData = () => {
-    if (page * pageSize < articlesLength) {
-      setPage((prevPage) => prevPage + 1);
-    }
-  };
-
-  const memoArticles = articles.slice(0, page * pageSize);
-
+const News = ({ articles, fetchMoreData, hasMore }) => {
   return (
-    <div>
-      {status === "loading" && (
-        <p className="w-full h-full flex justify-center items-center">
-          Loading...
-        </p>
-      )}
-      {status === "failed" && (
-        <p className="w-full h-full flex justify-center items-center">
-          {error}
-        </p>
-      )}
-      {status === "succeeded" && (
-        <div className="flex-1">
-          <div id="scrollableDiv" className="h-[70dvh] overflow-auto lg:h-[75dvh]">
-            <InfiniteScroll
-              dataLength={memoArticles.length}
-              next={fetchMoreData}
-              hasMore={page * pageSize < articlesLength}
-              loader={<p>Loading more articles...</p>}
-              scrollableTarget="scrollableDiv"
-              endMessage={
-                <p className="text-center">
-                  <b>You are out of articles...</b>
-                </p>
-              }
-            >
-              <ul className="my-3 grid grid-cols-1 gap-[25px] lg:grid-cols-3">
-                {memoArticles.map((article, index) => (
-                  <NewsCard article={article} key={index} />
-                ))}
-              </ul>
-            </InfiniteScroll>
-          </div>
-        </div>
-      )}
+    <div className="flex-1">
+      <div id="scrollableDiv" className="h-[70dvh] overflow-auto lg:h-[75dvh]">
+        <InfiniteScroll
+          dataLength={articles.length}
+          next={fetchMoreData}
+          hasMore={hasMore}
+          loader={<p>Loading more articles...</p>}
+          scrollableTarget="scrollableDiv"
+          endMessage={
+            <p className="text-center">
+              <b>You are out of articles...</b>
+            </p>
+          }
+        >
+          <ul className="my-3 grid grid-cols-1 gap-[25px] lg:grid-cols-3">
+            {articles.map((article, index) => (
+              <NewsCard article={article} key={index} />
+            ))}
+          </ul>
+        </InfiniteScroll>
+      </div>
     </div>
   );
 };
