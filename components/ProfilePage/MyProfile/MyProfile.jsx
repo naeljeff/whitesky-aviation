a54ddd0d3786139a7bcbb0bf652a4b7178f403ca
naeljeff <naelj15@gmail.com>
2024-08-09 15:21:15 +0700

    fixed size modal

--- components/ProfilePage/MyProfile/MyProfile.jsx
+++ components/ProfilePage/MyProfile/MyProfile.jsx
@@ -26,11 +26,11 @@ const MyProfile = ({ onClose }) => {
       >
         <div
           onClick={(e) => e.stopPropagation()}
-          className="relative w-11/12 h-4/5 rounded-lg bg-white shadow-2xl lg:h-3/4 lg:w-2/5 lg:min-w-[40%] lg:max-w-[40%]"
+          className="relative w-11/12 h-4/5 rounded-lg bg-white shadow-2xl lg:h-11/12 lg:w-2/5 lg:min-w-[40%] lg:max-w-[40%]"
         >
           <div className="h-full w-full flex flex-col items-center justify-between">
             <div className="w-full">
-              <div className="w-full flex justify-end items-center p-2 text-xl font-semibold text-blue-gray-900 lg:p-4 lg:text-2xl">
+              <div className="w-full flex justify-end items-center p-2 text-xl font-semibold text-blue-gray-900 lg:p-4 lg:text-xl">
                 <p className="w-full text-center -mr-7">My Profile</p>
                 <Link
                   href="/profile"
@@ -61,7 +61,7 @@ const MyProfile = ({ onClose }) => {
                 <div className="w-full h-20 flex flex-col justify-start items-center">
                   <div className="w-3/4 space-y-1 lg:space-y-2">
                     <p className="text-sm text-blue-gray-600">Name</p>
-                    <p className="text-xl font-bold px-1 lg:text-2xl">
+                    <p className="text-lg font-bold px-1 lg:text-xl">
                       {user?.name || ""}
                     </p>
                     <div className="border-b border-blue-gray-100" />
@@ -72,7 +72,7 @@ const MyProfile = ({ onClose }) => {
                 <div className="w-full h-20 flex flex-col justify-start items-center">
                   <div className="w-3/4 space-y-1 lg:space-y-2">
                     <p className="text-sm text-blue-gray-600">Email</p>
-                    <p className="text-xl font-bold px-1 lg:text-2xl">
+                    <p className="text-lg font-bold px-1 lg:text-xl">
                       {user?.email || ""}
                     </p>
                     <div className="border-b border-blue-gray-100" />
@@ -83,7 +83,7 @@ const MyProfile = ({ onClose }) => {
                 <div className="w-full h-20 flex flex-col justify-start items-center">
                   <div className="w-3/4 space-y-1 lg:space-y-2">
                     <p className="text-sm text-blue-gray-600">Phone</p>
-                    <p className="text-xl font-bold px-1 lg:text-2xl">
+                    <p className="text-lg font-bold px-1 lg:text-xl">
                       {user?.phone_number || ""}
                     </p>
                     <div className="border-b border-blue-gray-100" />
