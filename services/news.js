241fe958ae62c8a97565af54b065350796aa7f7d
naeljeff <naelj15@gmail.com>
2024-08-09 15:38:54 +0700

    added more timeout to news fetching

--- services/news.js
+++ services/news.js
@@ -6,7 +6,7 @@ export const fetchNewsData = async ({ category }) => {
   const apiURL = `https://newsapi.org/v2/everything?q=${category}&apiKey=${NEWS_KEY}`;
 
   try {
-    const { data } = await axios.get(apiURL);
+    const { data } = await axios.get(apiURL, { timeout: 10000 });
     return data;
   } catch (error) {
     console.log(`Error fetching news data: ${error.message}`);
