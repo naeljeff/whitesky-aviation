67a419fffe45e069382d28d3adb2cd5fe7af52dd
naeljeff <naelj15@gmail.com>
2024-08-09 15:29:04 +0700

    fixed key

--- services/news.js
+++ services/news.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const NEWS_KEY = process.env.NEXT_PUBLIC_NEWS_KEY || "";
+const NEWS_KEY = process.env.NEXT_PUBLIC_NEWS_KEY || "fec7990d0c864c979966598e2a688ca5";
 
 export const fetchNewsData = async ({ category }) => {
   const apiURL = `https://newsapi.org/v2/everything?q=${category}&apiKey=${NEWS_KEY}`;
