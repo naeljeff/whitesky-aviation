5f6dcc6e6408cb878e21ed9f672110b2b4ebd3be
Nathanael Jeffrey <naelj15@gmail.com>
2024-08-12 00:11:51 +0700

    move news fetching to backend

--- services/news.js
+++ services/news.js
@@ -1,13 +1,14 @@
 import axios from "axios";
 
-const NEWS_KEY = process.env.NEXT_PUBLIC_NEWS_KEY || "fec7990d0c864c979966598e2a688ca5";
+// const NEWS_KEY = process.env.NEXT_PUBLIC_NEWS_KEY || "fec7990d0c864c979966598e2a688ca5";
 
 export const fetchNewsData = async ({ category }) => {
-  const apiURL = `https://newsapi.org/v2/everything?q=${category}&apiKey=${NEWS_KEY}`;
+  const apiURL = `https://whitesky-aviation-api.vercel.app/api/v1/news/${category}`;
 
   try {
     const { data } = await axios.get(apiURL, { timeout: 10000 });
-    return data;
+    console.log(data);
+    return data.data;
   } catch (error) {
     console.log(`Error fetching news data: ${error.message}`);
     throw error;
